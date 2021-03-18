9a0ff7a34db853a3f1cc33c589a53429c44c5297
Märt Kalmo <mart.kalmo@gmail.com>
2021-03-18 14:56:24 +0200

    added test for hw6

---
+++ hw6.php
@@ -0,0 +1,157 @@
+<?php
+
+require_once 'vendor/php-test-framework/public-api.php';
+
+const BASE_URL = 'http://localhost:8080';
+
+function canSaveBooksWithSingleAuthor() {
+
+    $authorName = insertSampleAuthor();
+
+    gotoLandingPage();
+
+    clickBookFormLink();
+
+    $book = getSampleBook();
+
+    setTextFieldValue('title', $book->title);
+    setRadioFieldValue('grade', '5');
+    selectOptionWithText('author1', $authorName);
+
+    clickBookFormSubmitButton();
+
+    assertThat(getPageText(), containsStringOnce($book->title));
+    assertThat(getPageText(), containsStringOnce($authorName));
+}
+
+function canUpdateBooksWithSingleAuthor() {
+
+    $originalAuthorName = insertSampleAuthor();
+    $newAuthorName = insertSampleAuthor();
+
+    gotoLandingPage();
+
+    clickBookFormLink();
+
+    $bookTitle = getSampleBook()->title;
+
+    setTextFieldValue('title', $bookTitle);
+    selectOptionWithText('author1', $originalAuthorName);
+
+    clickBookFormSubmitButton();
+
+    clickLinkWithText($bookTitle);
+
+    assertThat(getFieldValue('title'), is($bookTitle));
+    assertThat(getSelectedOptionText('author1'), is($originalAuthorName));
+
+    selectOptionWithText('author1', $newAuthorName);
+
+    clickBookFormSubmitButton();
+
+    assertThat(getPageText(), containsStringOnce($newAuthorName));
+    assertThat(getPageText(), doesNotContainString($originalAuthorName));
+}
+
+function canSaveBooksWithMultipleAuthors() {
+
+    $authorName1 = insertSampleAuthor();
+    $authorName2 = insertSampleAuthor();
+
+    gotoLandingPage();
+
+    clickBookFormLink();
+
+    $bookTitle = getSampleBook()->title;
+
+    setTextFieldValue('title', $bookTitle);
+    selectOptionWithText('author1', $authorName1);
+    selectOptionWithText('author2', $authorName2);
+
+    clickBookFormSubmitButton();
+
+    assertThat(getPageText(), containsStringOnce($bookTitle));
+    assertThat(getPageText(), containsStringOnce($authorName1));
+    assertThat(getPageText(), containsStringOnce($authorName2));
+}
+
+function canUpdateBooksWithMultipleAuthors() {
+
+    $authorName1 = insertSampleAuthor();
+    $authorName2 = insertSampleAuthor();
+    $authorName3 = insertSampleAuthor();
+
+    gotoLandingPage();
+
+    clickBookFormLink();
+
+    $bookTitle = getSampleBook()->title;
+
+    setTextFieldValue('title', $bookTitle);
+    selectOptionWithText('author1', $authorName1);
+    selectOptionWithText('author2', $authorName2);
+
+    clickBookFormSubmitButton();
+
+    clickLinkWithText($bookTitle);
+
+    assertThat(getFieldValue('title'), is($bookTitle));
+    assertThat(getSelectedOptionText('author1'), is($authorName1));
+    assertThat(getSelectedOptionText('author2'), is($authorName2));
+
+    selectOptionWithText('author1', $authorName2);
+    selectOptionWithText('author2', $authorName3);
+
+    clickBookFormSubmitButton();
+
+    assertThat(getPageText(), containsStringOnce($authorName2));
+    assertThat(getPageText(), containsStringOnce($authorName3));
+    assertThat(getPageText(), doesNotContainString($authorName1));
+}
+
+function doesNotAllowSqlInjection() {
+
+    gotoLandingPage();
+
+    clickBookFormLink();
+
+    $bookTitle = getSampleBook()->title;
+
+    setTextFieldValue('title', $bookTitle);
+
+    $dangerousSymbols = " ; \" ' ";
+    forceFieldValue('grade', $dangerousSymbols);
+    forceFieldValue('isRead', $dangerousSymbols);
+    forceFieldValue('author1', $dangerousSymbols);
+    forceFieldValue('author2', $dangerousSymbols);
+
+    clickBookFormSubmitButton();
+
+    assertThat(getPageText(), containsString($bookTitle));
+}
+
+#Helpers
+
+function insertSampleAuthor() : string {
+
+    gotoLandingPage();
+
+    clickAuthorFormLink();
+
+    $author = getSampleAuthor();
+
+    setTextFieldValue('firstName', $author->firstName);
+    setTextFieldValue('lastName', $author->lastName);
+
+    clickAuthorFormSubmitButton();
+
+    return $author->firstName . ' ' . $author->lastName;
+}
+
+setBaseUrl(BASE_URL);
+setLogRequests(false);
+setLogPostParameters(false);
+setPrintStackTrace(false);
+setPrintPageSourceOnParseError(false);
+
+stf\runTests(new stf\PointsReporter([3 => 3, 5 => 5]));

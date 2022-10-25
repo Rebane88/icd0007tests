eba6404f395f471e08f0bbdf4c2e4c8d853dccf5
Märt Kalmo <mart.kalmo@gmail.com>
2022-10-25 10:43:39 +0300

    file count test fix

--- common.php
+++ common.php
@@ -2,21 +2,42 @@
 
 require_once 'vendor/php-test-framework/public-api.php';
 
-function repositoryDoesNotContainNonProjectFiles() {
-    global $argc, $argv;
+if ($argc < 2) {
+    die('Pass directory to scan as an argument' . PHP_EOL);
+} else {
+    $path = realpath($argv[1]);
+}
 
-    if ($argc < 2) {
-        die('Pass directory to scan as an argument' . PHP_EOL);
-    } else {
-        $path = realpath($argv[1]);
-    }
+if ($path === false) {
+    die('Argument is not a correct directory' . PHP_EOL);
+}
 
-    if ($path === false) {
-        die('Argument is not a correct directory' . PHP_EOL);
+function _repositoryDoesNotContainNonProjectPhpFiles() {
+    global $path;
+
+    $count = getFileCount($path, 'php');
+
+    if ($count > 12) {
+        fail(ERROR_C01, "Repository contains too many ($count) Php files (max 12)");
     }
+}
+
+function _repositoryDoesNotContainNonProjectHtmlFiles() {
+    global $path;
+
+    $count = getFileCount($path, 'html');
+
+    if ($count > 12) {
+        fail(ERROR_C01, "Repository contains too many ($count) Html files (max 12)");
+    }
+}
+
+#Helpers
+
+function getFileCount($path, $extension): int {
 
     $filter = function ($file) {
-        return ! preg_match('/^\\.\\/ex\\d$/', $file->getPathName());
+        return ! preg_match('/^(\\.\\/ex\\d)|vendor$/', $file->getPathName());
     };
 
     chdir($path);
@@ -25,26 +46,14 @@ function repositoryDoesNotContainNonProjectFiles() {
     $it = new RecursiveIteratorIterator(new RecursiveCallbackFilterIterator($it, $filter));
     $it = new RegexIterator($it, '/\.(\w+)$/i', RegexIterator::GET_MATCH);
 
-    $htmlCount = 0;
-    $phpCount = 0;
+    $count = 0;
     foreach($it as $each) {
-        $extension = strtolower($each[1]);
-        if ($extension === 'php') {
-            $phpCount++;
-        }
-        if ($extension === 'html') {
-            $htmlCount++;
+        if (strtolower($each[1]) === $extension) {
+            $count++;
         }
     }
 
-    if ($phpCount > 12) {
-        fail(ERROR_C01, "Repository contains too many ($phpCount) Php files (max 12)");
-    }
-
-    if ($htmlCount > 10) {
-        fail(ERROR_C01, "Repository contains too many ($htmlCount) Html files (max 10)");
-    }
-
+    return $count;
 }
 
 stf\runTests(new stf\PointsReporter([1 => 1]));
